b88dde2e12459a818ba73e91b0457ff256015069
max.hoch@hotmail.com <max.hoch@hotmail.com>
2015-12-16 22:56:56 +0100

    init project

---
+++ src/main/java/de/ops/cf/Application.java
@@ -0,0 +1,15 @@
+package de.ops.cf;
+import org.cloudfoundry.client.lib.*;
+
+
+public class Application {
+
+	public static void main(String[] args) {
+		String User=System.getenv("CFUser");
+		
+		String Password=System.getenv("CFPassword");
+		
+
+	}
+
+}
