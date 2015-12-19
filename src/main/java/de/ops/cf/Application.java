6e797ffad442dacfa9d87e25fa5e0ef81fd9388c
max.hoch@hotmail.com <max.hoch@hotmail.com>
2015-12-19 22:04:06 +0100

    log4j added

--- src/main/java/de/ops/cf/Application.java
+++ src/main/java/de/ops/cf/Application.java
@@ -1,14 +1,16 @@
 package de.ops.cf;
-import org.cloudfoundry.client.lib.*;
 
+import org.cloudfoundry.client.lib.*;
+import org.apache.log4j.Logger;
 
 public class Application {
 
+	private static final Logger logger = Logger.getLogger(Application.class);
+	
 	public static void main(String[] args) {
 		String User=System.getenv("CFUser");
-		
 		String Password=System.getenv("CFPassword");
-		
+		logger.info(String.format("Read System Variables: User: %s, password: xxx", "s"));
 
 	}
 
